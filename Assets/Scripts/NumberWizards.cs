f26e6a604f0896ea416c3b6ab3c5d249a75860f9
muhtarA <muhtarrn@gmail.com>
2018-05-09 11:31:03 +0700

    add basic c#

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizards : MonoBehaviour {
+
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame ()
+	{
+		max = max + 1;
+
+		print("Welcome to Number Wizard");
+		print("Pick a number in your head");
+		print("The highest number you can pick is " + max);
+		print("The lowest number you can pick is " + min);	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print("I won!");
+		}
+	}
+
+	void NextGuess ()
+	{
+		guess = (max + min) / 2;
+		print("Higher or lower than " + guess);
+		print("Up = higher, down = lower, return = equal");
+	}
+}
